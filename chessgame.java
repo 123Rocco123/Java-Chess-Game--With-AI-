104c86483c83072e69ec7c72f5ebcdcc212b044f
Rocco Petruccio <petruccio.rocco@gmail.com>
2021-12-23 09:49:24 +0000

    HashMap used to store the player's positions

--- chessgame.java
+++ chessgame.java
@@ -1,8 +1,34 @@
 // Java Chess Game
 import java.util.ArrayList;
-import java.util.Hashmap;
+import java.util.HashMap;
 
 class chessgame {
+  // The function below is used to store the location of each of the player pieces.
+  public static HashMap<String, Integer> playerPieces() {
+    HashMap<String, Integer> playerPiecesLocations = new HashMap<String, Integer>();
+
+    // Starting Positions of the Player
+    playerPiecesLocations.put("T1", 56);
+    playerPiecesLocations.put("H1", 57);
+    playerPiecesLocations.put("B1", 58);
+    playerPiecesLocations.put("Q1", 59);
+    playerPiecesLocations.put("K1", 60);
+    playerPiecesLocations.put("B2", 61);
+    playerPiecesLocations.put("H2", 62);
+    playerPiecesLocations.put("T2", 63);
+
+    playerPiecesLocations.put("P1", 48);
+    playerPiecesLocations.put("P2", 49);
+    playerPiecesLocations.put("P3", 50);
+    playerPiecesLocations.put("P4", 51);
+    playerPiecesLocations.put("P5", 52);
+    playerPiecesLocations.put("P6", 53);
+    playerPiecesLocations.put("P7", 54);
+    playerPiecesLocations.put("P8", 55);
+
+    return playerPiecesLocations;
+  }
+
   // The function below is used to create and print out the board.
   public static void Board() {
     ArrayList<String> chessBoard = new ArrayList<String>();
