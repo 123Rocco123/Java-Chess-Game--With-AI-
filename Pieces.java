c762589d44c01d0d8a68063c6a27097f27302e9b
Rocco Petruccio <petruccio.rocco@gmail.com>
2021-12-24 15:39:28 +0000

    Pieces Class Comments

--- Pieces.java
+++ Pieces.java
@@ -1,9 +1,9 @@
 class Pieces {
-  boolean isFree = false;
-
+  // The instance variables below are used to store the location of the piece, and the piece itself.
   int startingLocation;
   String piece;
 
+  // The function below is used to assign values to the instance variables above. 
   public void positionSetter(int start, String chessPiece) {
     this.startingLocation = start;
     this.piece = chessPiece;
