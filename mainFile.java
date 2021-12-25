02f784efb33e32b93def45aba14e5bd8718f26c0
Rocco Petruccio <petruccio.rocco@gmail.com>
2021-12-25 01:44:12 +0000

    Board Changes & Function Changes in Main

--- mainFile.java
+++ mainFile.java
@@ -1,12 +1,13 @@
 import java.util.Arrays;
 
 class mainFile {
-  public static void main(String[] args) {
-    Board test = new Board();
-    test.chessBoardSetter(0, 0, "test");
-
-      for (String[] row : test.chessBoard) {
+  public static void boardOutput(Board chessBoardObject) {
+      for (String[] row : chessBoardObject.chessBoard) {
         System.out.println(Arrays.toString(row));
       }
+  }
+
+  public static void main(String[] args) {
+
     }
   }
