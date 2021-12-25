a08e0765e2a114a953fad02f0cd75347a617e6e3
Rocco Petruccio <petruccio.rocco@gmail.com>
2021-12-25 01:36:10 +0000

    Reset

--- Board.java
+++ Board.java
@@ -1,22 +1,14 @@
-import java.util.ArrayList;
-
 class Board {
-  // The ArrayList below is used to contain the chessboard that the players are playing on.
-  ArrayList<String> ChessBoard = new ArrayList<String>();
+  String[][] chessBoard = {{"|__|", "|__|", "|__|", "|__|", "|__|", "|__|", "|__|", "|__|"},
+                           {"|__|", "|__|", "|__|", "|__|", "|__|", "|__|", "|__|", "|__|"},
+                           {"|__|", "|__|", "|__|", "|__|", "|__|", "|__|", "|__|", "|__|"},
+                           {"|__|", "|__|", "|__|", "|__|", "|__|", "|__|", "|__|", "|__|"},
+                           {"|__|", "|__|", "|__|", "|__|", "|__|", "|__|", "|__|", "|__|"},
+                           {"|__|", "|__|", "|__|", "|__|", "|__|", "|__|", "|__|", "|__|"},
+                           {"|__|", "|__|", "|__|", "|__|", "|__|", "|__|", "|__|", "|__|"},
+                           {"|__|", "|__|", "|__|", "|__|", "|__|", "|__|", "|__|", "|__|"}};
 
-  // The instance variables below are used to contain an empty spot, and an object of the "Pieces" class.
-  String emptySpace = "|__|";
-  Pieces freeSpace = new Pieces();
-
-  // The function below is used to create a completely empty board.
-  public void BoardSetter() {
-    for (int i = 0; i < 64; i++) {
-      ChessBoard.add(emptySpace);
-    }
-  }
-
-  // The function below is used with the BoardSetter function, where it changes the empty spots to the ones containing pieces.
-  public void pieceAddition(int indexPosition, String pieceToAdd) {
-    ChessBoard.set(indexPosition, pieceToAdd);
+  public void chessBoardSetter(int arrayIndex, int indexInArray, String newValue) {
+    this.chessBoard[arrayIndex][indexInArray] = newValue;
   }
 }
