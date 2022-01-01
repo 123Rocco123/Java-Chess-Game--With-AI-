1b57f837ad0146211a2ebecf65aa939dbe682ac7
Rocco Petruccio <petruccio.rocco@gmail.com>
2022-01-01 16:32:27 +0000

    Rook Movement Definition

--- rook.java
+++ rook.java
@@ -1,3 +1,12 @@
 class rook extends Pieces {
-  
+  @Override
+  public boolean allowedMoves(int newRow, int newColumn) {
+    if ((newRow < this.row || newRow > this.row) && newColumn == this.column) {
+      return true;
+    } else if ((newColumn < this.column || newColumn > this.column) && newRow == this.row) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
