bd5abb27a50596fb407641cdc7acb2049ab9a6d9
Rocco Petruccio <petruccio.rocco@gmail.com>
2022-01-01 18:00:03 +0000

    Comments

--- Pieces.java
+++ Pieces.java
@@ -6,22 +6,28 @@ class Pieces {
 
   String[][] chessBoard;
 
+  // The function is used to assign the rows to the chessboard, by changing the instance variable in the class.
   public void rowAssignment(int rowNow) {
     this.row = rowNow;
   }
 
+  // The function is used to assign the columns to the chessboard, by changing the instance variable in the class.
   public void columnAssignment(int columnNow) {
     this.column = columnNow;
   }
 
+  // The function is used to assign the array containing the chessboard, by changing the instance variable in the class.
   public void chessBoardAssignemnt(String[][] chessBoardToAssign) {
     this.chessBoard = chessBoardToAssign;
   }
 
+  // The function is used to assign the chess piece to the chessboard, by changing the instance variable in the class.
   public void chessPieceLook(String chessPieceToAssign) {
     this.chessPiece = chessPieceToAssign;
   }
 
+  // The function is used as a skeleton structure for the allowed moves that the chess pieces are able to do.
+     // The returned value of the function is then used to see if the move that the uesr inputted is valid or not.
   public boolean allowedMoves(int newRow, int newColumn) {
     if ((newRow == (this.row + 1)) && (newColumn == column)) {
       return true;
@@ -30,6 +36,7 @@ class Pieces {
     }
   }
 
+  // The function below is used to re-assign the value in the Array[][] with the piece using the instance variables in the class.
   public void setSpace(int newRow, int newColumn) {
     this.chessBoard[newRow][newColumn] = this.chessPiece;
   }
--- bishop.java
+++ bishop.java
@@ -1,3 +1,5 @@
+// Bishop class used to set the rules for bishop movemenet.
+
 class bishop extends Pieces {
   @Override
   public boolean allowedMoves(int newRow, int newColumn) {
