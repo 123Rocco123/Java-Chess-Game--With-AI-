3d385bd4da1268796fb18bdcb4975c643851eb2b
123Rocco123 <petruccio.rocco@gmail.com>
2022-01-04 22:51:24 +0000

    Knight Attack Functionality

--- knight.java
+++ knight.java
@@ -3,56 +3,59 @@
 class knight extends Pieces {
   @Override
   public boolean allowedMoves(int newRow, int newColumn) {
-    // Horse moves forward and to the left.
-    if ((newRow == (this.row - 2)) && newColumn == this.column - 1  && this.chessBoard[(this.row - 2)][this.column - 1].equals("|__|")) {
-      this.row = newRow;
-      this.column = newColumn;
+    for  (int i = 0; i < AIPieces.length; i++) {
+    if (this.chessBoard[newRow][newColumn].equals("|__|") || this.chessBoard[newRow][newColumn].equals(AIPieces[i])) {
+      if ((newRow == (this.row - 2)) && newColumn == this.column - 1) {
+        this.row = newRow;
+        this.column = newColumn;
 
-      return true;
-    } // Horse moves forward and to the right.
-    else if ((newRow == (this.row - 2)) && newColumn == this.column + 1 && this.chessBoard[(this.row - 2)][this.column + 1].equals("|__|")) {
-      this.row = newRow;
-      this.column = newColumn;
+        return true;
+      } // Horse moves forward and to the right.
+      else if ((newRow == (this.row - 2)) && newColumn == this.column + 1) {
+        this.row = newRow;
+        this.column = newColumn;
 
-      return true;
-    } // Horse moves down and to the left.
-    else if ((newRow == (this.row + 2)) && newColumn == this.column - 1 && this.chessBoard[(this.row + 2)][this.column - 1].equals("|__|")) {
-      this.row = newRow;
-      this.column = newColumn;
+        return true;
+      } // Horse moves down and to the left.
+      else if ((newRow == (this.row + 2)) && newColumn == this.column - 1) {
+        this.row = newRow;
+        this.column = newColumn;
 
-      return true;
-    } // Horse moves down and to the right.
-    else if ((newRow == (this.row + 2)) && newColumn == this.column + 1 && this.chessBoard[(this.row + 2)][this.column + 1].equals("|__|")) {
-      this.row = newRow;
-      this.column = newColumn;
+        return true;
+      } // Horse moves down and to the right.
+      else if ((newRow == (this.row + 2)) && newColumn == this.column + 1) {
+        this.row = newRow;
+        this.column = newColumn;
 
-      return true;
-    } // Horse moves down and to the left (further left).
-    else if ((newRow == (this.row + 1)) && newColumn == this.column - 2 && this.chessBoard[(this.row + 1)][this.column - 2].equals("|__|")) {
-      this.row = newRow;
-      this.column = newColumn;
+        return true;
+      } // Horse moves down and to the left (further left).
+      else if ((newRow == (this.row + 1)) && newColumn == this.column - 2) {
+        this.row = newRow;
+        this.column = newColumn;
 
-      return true;
-    } // Horse moves down and to the right (further right).
-    else if ((newRow == (this.row + 1)) && newColumn == this.column + 2 && this.chessBoard[(this.row + 1)][this.column + 2].equals("|__|")) {
-      this.row = newRow;
-      this.column = newColumn;
+        return true;
+      } // Horse moves down and to the right (further right).
+      else if ((newRow == (this.row + 1)) && newColumn == this.column + 2) {
+        this.row = newRow;
+        this.column = newColumn;
 
-      return true;
-    } // Horse moves up and to the left (further left).
-    else if ((newRow == (this.row - 1)) && newColumn == this.column - 2 && this.chessBoard[(this.row - 1)][this.column - 2].equals("|__|")) {
-      this.row = newRow;
-      this.column = newColumn;
+        return true;
+      } // Horse moves up and to the left (further left).
+      else if ((newRow == (this.row - 1)) && newColumn == this.column - 2) {
+        this.row = newRow;
+        this.column = newColumn;
 
-      return true;
-    } // Horse moves up and to the right (further right).
-    else if ((newRow == (this.row - 1)) && newColumn == this.column + 2 && this.chessBoard[(this.row - 1)][this.column + 2].equals("|__|")) {
-      this.row = newRow;
-      this.column = newColumn;
+        return true;
+      } // Horse moves up and to the right (further right).
+      else if ((newRow == (this.row - 1)) && newColumn == this.column + 2) {
+        this.row = newRow;
+        this.column = newColumn;
 
-      return true;
-    } else {
-      return false;
+        return true;
+      }
     }
+  }
+
+    return false;
   }
 }
