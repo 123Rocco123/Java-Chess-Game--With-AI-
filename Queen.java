7c7cd51edc357af39b4df213d7c7c9e37c331597
123Rocco123 <petruccio.rocco@gmail.com>
2022-01-10 13:54:01 +0000

    Queen Attack Move Functions

--- Queen.java
+++ Queen.java
@@ -108,7 +108,105 @@ class Queen extends Pieces {
         for (int x = 0; x < AIPieces.length; x++) {
           // The if statement is used to check if the location that the user inputted is in fact a location with the enemy team's chess piece.
           if (chessBoard[newRow][newColumn].equals(AIPieces[x])) {
+            for (int b = 0; b <= 8; b++) {
+              // Move down to an attack space. (Works).
+              if (newRow == row + b && newColumn == column) {
+                for (int z = 1; z < (newRow - row); z++) {
+                  if (!(chessBoard[newRow - z][column].equals("|__|"))) {
+                    System.out.println(chessBoard[newRow - z][column]);
+                    return false;
+                  }
+                }
+                this.row = newRow;
+                this.column = newColumn;
 
+                return true;
+              } // Move up to a attack space. (Works)
+              else if (newRow == row - b && newColumn == column) {
+                for (int z = 1; z < (row - newRow); z++) {
+                  if (!(chessBoard[newRow + z][column].equals("|__|"))) {
+                    System.out.println(chessBoard[newRow + z][column]);
+                    return false;
+                  }
+                }
+                this.row = newRow;
+                this.column = newColumn;
+
+                return true;
+              } // Move left to a attack space. (Works)
+              else if (newRow == row && newColumn == column - b) {
+                for (int z = 1; z < (column - newColumn); z++) {
+                  if (!(chessBoard[newRow][newColumn + z].equals("|__|"))) {
+                    System.out.println(chessBoard[newRow][newColumn + z]);
+                    return false;
+                  }
+                }
+                this.row = newRow;
+                this.column = newColumn;
+
+                return true;
+              } // Move right to a attack space. (Works)
+              else if (newRow == row && newColumn == column + b) {
+                for (int z = 1; z < (newColumn - column); z++) {
+                  if (!(chessBoard[newRow][newColumn - z].equals("|__|"))) {
+                    System.out.println(chessBoard[newRow][newColumn - z]);
+                    return false;
+                  }
+                }
+                this.row = newRow;
+                this.column = newColumn;
+
+                return true;
+              } // Move right and up to a attack space. (Works)
+              else if (newRow == row - b && newColumn == column + b) {
+                for (int z = 1; z < (row - newRow); z++) {
+                  if (!(chessBoard[newRow + z][newColumn - z].equals("|__|"))) {
+                    System.out.println(chessBoard[newRow + z][newColumn - z]);
+                    return false;
+                  }
+                }
+                this.row = newRow;
+                this.column = newColumn;
+
+                return true;
+              } // Move left and up to a attack space. (Works)
+              else if (newRow == row - b && newColumn == column - b) {
+                for (int z = 1; z < (row - newRow); z++) {
+                  if (!(chessBoard[newRow + z][newColumn + z].equals("|__|"))) {
+                    System.out.println(chessBoard[newRow + z][newColumn + z]);
+                    return false;
+                  }
+                }
+                this.row = newRow;
+                this.column = newColumn;
+
+                return true;
+              } // Move down and right to a attack space. (Works)
+              else if (newRow == row + b && newColumn == column + b) {
+                for (int z = 1; z < (newRow - row); z++) {
+                  if (!(chessBoard[newRow - z][newColumn - z].equals("|__|"))) {
+                    System.out.println(chessBoard[newRow - z][newColumn - z]);
+                    return false;
+                  }
+                }
+                this.row = newRow;
+                this.column = newColumn;
+
+                return true;
+              } // Move down and left to a attack space. (Works)
+              else if (newRow == row + b && newColumn == column - b) {
+                for (int z = 1; z < (newRow - row); z++) {
+                  if (!(chessBoard[newRow - z][newColumn + z].equals("|__|"))) {
+                    System.out.println(chessBoard[newRow - z][newColumn + z]);
+                    return false;
+                  }
+                }
+                this.row = newRow;
+                this.column = newColumn;
+
+                return true;
+              }
+            }
           }
         }
       }
