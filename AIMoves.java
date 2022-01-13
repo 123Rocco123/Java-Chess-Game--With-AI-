7f91cdffceda9a2ba571134d5d01f681932c0105
123Rocco123 <petruccio.rocco@gmail.com>
2022-01-13 02:11:39 +0000

    Comments

--- AIMoves.java
+++ AIMoves.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 
 class AIMoves {
+  // The ArrayList contains the pawn objects.
   ArrayList<Pawn> pawnObjects = new ArrayList<Pawn>();
 
   String[][] chessBoard;
@@ -26,10 +27,12 @@ class AIMoves {
                            "|K1|", "|K2|",
                            "|Q1|", "|KI|"};
 
+  // Set the chessBoard to the one that the player plays on.
   public void chessBoardSetterFunc(String[][] boardToPLayOn) {
     this.chessBoard = boardToPLayOn;
   }
 
+  // This re-assigns the values of the instnace variables to the ones that were defined in the main class.
   public void objectPawnSetter(Pawn pawn, Pawn pawn2, Pawn pawn3, Pawn pawn4, Pawn pawn5, Pawn pawn6, Pawn pawn7, Pawn pawn8) {
     this.pawn9 = pawn;
     this.pawn10 = pawn2;
@@ -57,10 +60,13 @@ class AIMoves {
   // The function below is used to defend against the players team's chess pieces.
      // The for loops are used to iterate throughout the ches board and see which pieces are the computers and which are the player's.
   public void defendFunction() {
+    // The for loop is used to iterate throughout the entire chessBoard.
     for (int i = 0; i < chessBoard.length; i++) {
       for (int x = 0; x < chessBoard.length; x++) {
+        // The second set of for loops below is used to iterate through the AIPieces to see which spot contains one of the AI's chess pieces.
         for (int z = 0; z < AIPieces.length; z++) {
           if (chessBoard[i][x].equals(AIPieces[z])) {
+            // The if statement below is used to check if the piece's is a pawn or not. 
             if (z <= 7) {
               pawnObjectTest(pawnObjects.get(z));
             }
